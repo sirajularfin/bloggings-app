a581993dc4b232af3f3e74c70f5f0d8e864f173f
Sirajul Arfin <sirajul.arfin@nagarro.com>
2024-03-04 13:40:05 +0530

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  printWidth: 120,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
