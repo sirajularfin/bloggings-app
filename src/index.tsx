245ba66a02ff4976bbfbb7ea6acbfbce55e3ee97
sirajularfin <sirajularfin@gmail.com>
2024-10-08 14:20:13 +0530

    Add react-native-splash-screen package

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AppNavigator from './navigation/appNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+	useEffect(() => {
+		// Hide the splash screen when the component mounts.
+		SplashScreen.hide();
+	}, []);
+
 	return (
 		<SafeAreaView style={{flex: 1}}>
 			<AppNavigator />
