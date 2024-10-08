c549b94b700199fbce96359c119cf85a161be2bb
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2024-10-08 17:06:12 +0530

    Add Splash Screen in IOS and update basic configuration (#4)
    * Remove splash screen code
    
    * Add react-native-splash-screen package
    
    * Add Splash screen in IOS
    
    * Add eslint configuration
    
    * Add a basic configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AppNavigator from './navigation/appNavigator';
+import SplashScreen from 'react-native-splash-screen';
+import {StyleSheet} from 'react-native';
 
 const App = () => {
+	useEffect(() => {
+		// Hide the splash screen when the component mounts.
+		SplashScreen.hide();
+	}, []);
+
 	return (
-		<SafeAreaView style={{flex: 1}}>
+		<SafeAreaView style={styles.container}>
 			<AppNavigator />
 		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
 
 export default App;
